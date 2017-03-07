6f0c91d6348396f10d4fbd2f9d80b60c679ac4d4
Julien Roy <julien.s.roy@gmail.com>
2017-03-06 21:20:56 -0800

    fixing warrantyrtooltip

--- assets/js/AQL JS ES6/productJS/warrantyTooltip.js
+++ assets/js/AQL JS ES6/productJS/warrantyTooltip.js
@@ -1,4 +1,10 @@
 // Warranty Tooltip Popup
-$('#warranty-content').on('click', () => {
-	$('#warranty-popup').toggle();
+const popup = document.querySelector('#warranty-popup');
+
+document.querySelector('#warranty-content').addEventListener('click', () => {
+    if (popup.classList.contains('active')) {
+        popup.classList.remove('active');
+    } else {
+        popup.classList.add('active');
+    }
 });
