d1caeab80ed9283f94203fc9a236b378dd766aaf
Brandon <theeternalvigil@gmail.com>
2017-03-15 19:15:02 -0700

    implimented new universal google analytics tracking code

--- assets/js/AQL-JS-ES6/globalJS/googleAnalytics.js
+++ assets/js/AQL-JS-ES6/globalJS/googleAnalytics.js
@@ -1,17 +1,18 @@
 // Google Analytics Tracking
- var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-39564069-1']);
-  _gaq.push(['_trackPageview']);
-
-  ( () => {
-    let ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+let _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-39564069-1']);
+_gaq.push(['_trackPageview']);
+(() => {
+    let ga = document.createElement('script');
+    ga.type = 'text/javascript';
+    ga.async = true;
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    let s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-  /*<![CDATA[ */
-  var google_conversion_id = 1069749593,
-  google_custom_params = window.google_tag_params,
-  google_remarketing_only = true,
-  google_conversion_format = 3;
-  /* ]]>*/
+    let s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(ga, s);
+})();
+/*<![CDATA[ */
+let google_conversion_id = 1069749593,
+    google_custom_params = window.google_tag_params,
+    google_remarketing_only = true,
+    google_conversion_format = 3;
+/* ]]>*/
