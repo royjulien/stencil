a2149f4a0a74c622df99626ba59c1c9aab7c13ff
Brandon <theeternalvigil@gmail.com>
2017-03-22 19:35:38 -0700

    accessory tab js modified to included LED rope light accessories, styling added to accessory tabs

--- assets/js/theme/product/accessoryTabContent.js
+++ assets/js/theme/product/accessoryTabContent.js
@@ -1,16 +1,94 @@
 // Accessory Page JS
+function clearTabContent(tab){
+	tab.innerText = '';
+}
+
+function getJSON(url, callback) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = () => {
+        const status = xhr.status;
+        if (status === 200) {
+            callback(null, xhr.response);
+        } else {
+            callback(status);
+        }
+    };
+    xhr.send(null);
+}
+
+function createHTML(data) {
+	const container = document.createElement('div');
+
+	const tabTitle = document.createElement('span');
+	tabTitle.className = 'product-title';
+	tabTitle.innerText = data.header;
+	container.append(tabTitle);
+
+
+	const ulElement = document.createElement('ul');
+	ulElement.className = 'accessorytab';
+
+	// for loop start point
+	for(let i = 0; i < data.items.length; i++) {
+	let cardLink = document.createElement('a');
+	cardLink.target='_blank';
+	cardLink.href = data.items[i].itemLink;
+
+	let liElement = document.createElement('li');
+	liElement.className = 'accessorytab';
+
+	let liImage = document.createElement('img');
+	liImage.src = data.items[i].itemImg;
+	liElement.append(liImage);
+	liElement.append(data.items[i].itemName);
+
+	cardLink.append(liElement);
+	ulElement.append(cardLink);
+	}
+	// for loop end point
+
+	container.append(ulElement);
+	return container;
+}
+
 const accessoryTabContent = document.querySelector('#Things') || document.querySelector('#Accessories');
 
 if(accessoryTabContent){
-	const tabContent = accessoryTabContent.innerText;
+	let tabContent = accessoryTabContent.innerText;
+	tabContent = tabContent.split('.')[1];
+	tabContent = tabContent.split('%%')[0];
 	console.log(tabContent);
 
 	switch(tabContent) {
-		case '%%Panel.LEDRopeLightAccessories%%':
+		case 'LEDRopeLightAccessories':
 			console.log('rope light accessories');
+			const requestURL = 'http://www.affordablequalitylighting.com/template/LEDRopeLightAccessories.json';
+			getJSON(requestURL, (error, data) => {
+				clearTabContent(accessoryTabContent);
+				console.log(data);
+				const newContent = createHTML(data);
+				accessoryTabContent.append(newContent);
+			});
 			break;
-		case '%%Panel.OutdoorAccessories%%':
+		case 'OutdoorAccessories':
 			console.log('outdoor accessories');
+			const requestURL = 'http://www.affordablequalitylighting.com/template/outdoorAccessories.json';
+			getJSON(requestURL, (error, data) => {
+				clearTabContent(accessoryTabContent);
+				console.log(data);
+
+				const newContent = createHTML(data);
+				console.log(newContent);
+				accessoryTabContent.append(newContent);
+			});
+			break;
+		case '':
+			console.log('underwater accessories');
+			break;
+		case '12VRopeLightAccessories':
+			console.log('low voltage rope light accessories');
 			break;
 		default:
 			console.log('no accessories'); 
