9c520bd7bdb5de408c1926d3d8a2e141a3d5249c
Brandon <theeternalvigil@gmail.com>
2017-03-29 15:59:13 -0700

    modified altProduct.js logic, added basic styling to alt-container DOM element

--- assets/js/theme/product/altProduct.js
+++ assets/js/theme/product/altProduct.js
@@ -13,26 +13,43 @@ function getJSON(url, callback) {
     };
     xhr.send(null);
 }
-const productSKU = document.querySelector('#productSKU') || '';
-if (document.querySelector('#form-action-addToCart')) {
-    const isInStock = window.BCData.product_attributes.instock || true;
+console.log('page loaded');
+if (window.BCData.product_attributes) {
+    console.log('landed on a product page');
+    const stockStatus = BCData.product_attributes.instock;
+    console.log(stockStatus);
 
-    if (isInStock === false) {
+    if (stockStatus === false) {
+        console.log('product is not in stock');
+        const productSKU = BCData.product_attributes.sku;
+        console.log(productSKU);
+
         const altContainer = document.querySelector('.alt-container');
-        const requestURL = 'http://www.affordablequalitylighting.com/content/alt-products.json';
+        const requestURL = 'http://www.affordablequalitylighting.com/content/json/alt-products.json';
 
         getJSON(requestURL, (error, data) => {
             if (error) {
-                // console.log("Error has occured.");
+                console.log(error);
             } else {
+                console.log(data);
                 for (let i = data.length - 1; i >= 0; i--) {
                     if (data[i].sku === productSKU) {
-                        altContainer.innerText = data[i].name;
-                        altContainer.style.display = 'block';
-                        altContainer.style.cursor = 'pointer';
+                        const containerHeader = document.createElement('h3');
+                        containerHeader.innerText = 'Recommended Alternative:';
+                        altContainer.append(containerHeader);
+
                         const altProductImg = document.createElement('img');
                         altProductImg.setAttribute('src', data[i].imagePath);
                         altContainer.append(altProductImg);
+
+                        const containerText = document.createElement('p');
+                        containerText.className = 'name';
+                        containerText.innerText = data[i].name;
+                        altContainer.append(containerText);
+
+                        altContainer.style.display = 'block';
+                        altContainer.style.cursor = 'pointer';
+
                         const destinationURL = data[i].url;
                         altContainer.addEventListener('click', () => {
                             window.location.href = destinationURL;
@@ -43,3 +60,68 @@ if (document.querySelector('#form-action-addToCart')) {
         });
     }
 }
+/*
+if (isInStock === false) {
+console.log('is not in stock');
+const altContainer = document.querySelector('.alt-container');
+const requestURL = 'http://www.affordablequalitylighting.com/content/json/alt-products.json';
+
+getJSON(requestURL, (error, data) => {
+if (error) {
+console.log("Error has occured.");
+} else {
+console.log(data);
+for (let i = data.length - 1; i >= 0; i--) {
+if (data[i].sku === productSKUtext) {
+altContainer.innerText = 'We Recommend:';
+altContainer.innerText += data[i].name;
+altContainer.style.display = 'block';
+altContainer.style.cursor = 'pointer';
+const altProductImg = document.createElement('img');
+altProductImg.setAttribute('src', data[i].imagePath);
+altContainer.append(altProductImg);
+const destinationURL = data[i].url;
+altContainer.addEventListener('click', () => {
+window.location.href = destinationURL;
+}, true);
+}
+}
+}
+});
+}
+*/
+
+/*
+if (buyButton) {
+console.log('add to cart found');
+const isInStock = window.BCData.product_attributes.instock;
+
+if (isInStock === false) {
+console.log('is not in stock');
+const altContainer = document.querySelector('.alt-container');
+const requestURL = 'http://www.affordablequalitylighting.com/content/json/alt-products.json';
+
+getJSON(requestURL, (error, data) => {
+if (error) {
+console.log("Error has occured.");
+} else {
+console.log(data);
+for (let i = data.length - 1; i >= 0; i--) {
+if (data[i].sku === productSKUtext) {
+altContainer.innerText = 'We Recommend:';
+altContainer.innerText += data[i].name;
+altContainer.style.display = 'block';
+altContainer.style.cursor = 'pointer';
+const altProductImg = document.createElement('img');
+altProductImg.setAttribute('src', data[i].imagePath);
+altContainer.append(altProductImg);
+const destinationURL = data[i].url;
+altContainer.addEventListener('click', () => {
+window.location.href = destinationURL;
+}, true);
+}
+}
+}
+});
+}
+} */
