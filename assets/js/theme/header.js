588a9c70459816f87ffc01a1e3f1a7decebf87b1
Julien Roy <julien.s.roy@gmail.com>
2017-09-28 17:57:59 -0700

    fixing and redesigning menu; adding top header phone number;

--- assets/js/theme/header.js
+++ assets/js/theme/header.js
@@ -13,9 +13,9 @@ if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
 }
 
 if (windowWidth <= 801) {
-    $('.main-menu-cat').prepend('<div class="main-menu-cat-bracket">+</div>');
-    $('.main-menu-sub-cat-container').parent().prepend('<div class="main-menu-cat-bracket">+</div>');
-    $('.main-menu-cat > ul, .main-menu-sub-cat-container').prepend('<div class="main-menu-cat-back">Back</div>');
+    $('.main-menu-cat').prepend('<div class="main-menu-cat-bracket"></div>');
+    $('.main-menu-sub-cat-container').parent().prepend('<div class="main-menu-cat-bracket"></div>');
+    $('.main-menu-cat > ul, .main-menu-sub-cat-container').prepend('<div class="main-menu-cat-back">back</div>');
 
     $('.main-menu-cat-bracket').on('click', (e) => {
         $(e.currentTarget).siblings('ul').addClass('active');
