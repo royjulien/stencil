9ca1b3dbdf9016908f4df6ccfca0b7f2a8d6f035
Brandon <theeternalvigil@gmail.com>
2018-01-24 16:04:26 -0800

    modified spacing for npm linting purposes, updated config bundle version

--- assets/js/theme/product/accessoryTabContent.js
+++ assets/js/theme/product/accessoryTabContent.js
@@ -224,7 +224,7 @@ if (accessoryTabContent) {
                 const newContent = createHTML(data);
                 accessoryTabContent.appendChild(newContent);
             });
-        break;
+            break;
         default:
             clearTabContent(accessoryTabContent);
             accessoryTabContent.innerText = 'Coming Soon!';
