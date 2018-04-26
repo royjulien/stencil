e97d35848e1fbad4939ef3e08094c62183822918
Jongsoo Yoon <jongsoojong@gmail.com>
2018-04-25 17:13:35 -0700

    cleaned up code; trying to make the button close on click

--- assets/js/theme/global/checkbox-closer.js
+++ assets/js/theme/global/checkbox-closer.js
@@ -1,23 +1,26 @@
-window.onclick = function(event) {
-
-    if (!event.target.matches('.dropdown-label-checkbox')) {
-        var allButtons = document.getElementsByClassName('dropdown-label-checkbox');
-
-
-        for (var i = 0; i < allButtons.length; i++) {
-            allButtons[i].checked = true;
-        }
-
-    }
-
-}
+var allButtons = document.getElementsByClassName('dropdown-label-checkbox');
 var labelButton = document.querySelectorAll('.dropdown-button');
-
 var dropdownOptions = document.querySelectorAll('#drop-down--swatch');
+
+
+window.onclick = function(event) {
+    
+        if (!event.target.matches('.dropdown-label-checkbox')) {
+            console.log(event.target)
+    
+            for (var i = 0; i < allButtons.length; i++) {
+                allButtons[i].checked = true;
+            } 
+        
+        }
+        
+}
+
 
 for(var i = 0; i < dropdownOptions.length; i++) {
     dropdownOptions[i].addEventListener("click", function(event){
         var targetElement = event.target || event.srcElement;
+        if(targetElement.innerHTML) 
         var newDropDown = targetElement.innerHTML;
         labelButton[0].innerHTML = newDropDown + '<div class="arrow-down"></div>';
 })  
