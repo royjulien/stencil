0cfe0f45c907275d426aa087122bee7f42864840
Jongsoo Yoon <jongsoojong@gmail.com>
2018-04-26 11:11:39 -0700

    removed checkbox, and uses js now to toggle

--- assets/js/theme/global/checkbox-closer.js
+++ assets/js/theme/global/checkbox-closer.js
@@ -1,20 +1,40 @@
-const allButtons = document.getElementsByClassName('dropdown-label-checkbox'),
-      labelButton = document.querySelectorAll('.dropdown-button'),
+const labelButton = document.querySelectorAll('.dropdown-button'),
       dropdownOptions = document.querySelectorAll('#drop-down--swatch');
 
+
+// closes all dropdowns on outside clicks
+
 window.onclick = (event) => {
-  // close dropdown when user clicks anywhere but the dropdown
-  if (!event.target.matches('.dropdown-label-checkbox')) {
-    for (var i = allButtons.length - 1; i >= 0; i--) {
-      allButtons[i].checked = true;
+  let dropdownContent = document.querySelectorAll('.dropdown-content');
+  let targetElement = event.target || event.srcElement;
+
+  if (!targetElement.matches('.dropdown-button')) {
+    for (var i = dropdownContent.length - 1; i >= 0; i--) {
+      dropdownContent[i].className = 'dropdown-content-hide';
     }
   }
 }
 
 
-for (var i = dropdownOptions.length - 1; i >= 0; i--) {
+// opens and closes the dropdown on click of the button
+
+for (let i = labelButton.length - 1; i >= 0; i--) {
+  labelButton[i].addEventListener('click', (event) => {
+    let targetElement = event.target || event.srcElement;
+    let targetElementClass = targetElement.nextElementSibling;
+    if (targetElementClass.className === 'dropdown-content-hide') {
+      targetElementClass.className = 'dropdown-content'
+    } else {
+      targetElementClass.className = 'dropdown-content-hide';
+    }
+
+  })
+}
+
+// changes the dropdown button
+for (let i = dropdownOptions.length - 1; i >= 0; i--) {
   dropdownOptions[i].addEventListener('click', (event) => {
-    var targetElement = event.target || event.srcElement;
+    let targetElement = event.target || event.srcElement;
 
     if (targetElement.innerHTML) {
       labelButton[0].innerHTML = `${targetElement.innerHTML} <div class="arrow-down"></div>`;
