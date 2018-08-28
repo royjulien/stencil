19576c9d93976bfa764f10022eecb599df75151b
Julien Roy <julien.s.roy@gmail.com>
2018-08-28 12:13:59 -0700

    removing mobile main menu preventions

--- assets/js/theme/header.js
+++ assets/js/theme/header.js
@@ -6,12 +6,6 @@ $('[data-mobile-menu-toggle]').on('click', () => {
     $('#main-menu').toggleClass('active');
 });
 
-if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-    $('.main-menu-cat > a, .main-menu-cat-vertical-left > .main-menu-sub-cat > a').click((e) => {
-        e.preventDefault();
-    });
-}
-
 if (windowWidth <= 801) {
     $('.main-menu-cat').prepend('<div class="main-menu-cat-bracket"></div>');
     $('.main-menu-sub-cat-container').parent().prepend('<div class="main-menu-cat-bracket"></div>');
