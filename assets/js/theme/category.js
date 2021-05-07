152e34e653bd9026c84e52cbcef012b1cc9d09d2
Julien Roy <julien.s.roy@gmail.com>
2021-05-07 09:51:10 -0700

    not fixing category

--- assets/js/theme/category.js
+++ assets/js/theme/category.js
@@ -21,6 +21,58 @@ export default class Category extends CatalogPage {
             hooks.on('sortBy-submitted', this.onSortBySubmit);
         }
 
+        // // AHHHH lame js here
+        // var slideIndex = 1;
+        // var myTimer;
+        // var slideshowContainer;
+
+        // let plusSlides = (n) => {
+        //     clearInterval(myTimer);
+        //     n < 0 ? showSlides(slideIndex -= 1) : showSlides(slideIndex += 1);
+        //     n === -1 ? myTimer = setInterval(plusSlides(n + 2), 4000) : myTimer = setInterval(plusSlides(n + 1), 4000);
+        // }
+
+        // let currentSlide = (n) => {
+        //     clearInterval(myTimer);
+        //     myTimer = setInterval(plusSlides(n + 1), 4000);
+        //     showSlides(slideIndex = n);
+        // }
+
+        // let showSlides = (n) => {
+        //     var i;
+        //     var slides = document.getElementsByClassName("mySlides");
+        //     var dots = document.getElementsByClassName("slide-dot");
+        //     if (n > slides.length) {slideIndex = 1}
+        //     if (n < 1) {slideIndex = slides.length}
+        //     for (i = 0; i < slides.length; i++) {
+        //         slides[i].style.display = "none";
+        //     }
+        //     for (i = 0; i < dots.length; i++) {
+        //         dots[i].className = dots[i].className.replace(" active-slide", "");
+        //     }
+        //     slides[slideIndex-1].style.display = "block";
+        //     dots[slideIndex-1].className += " active-slide";
+        // }
+
+        // let pause = () => {
+        //     clearInterval(myTimer);
+        // }
+
+        // let resume = () =>{
+        //     clearInterval(myTimer);
+        //     myTimer = setInterval(plusSlides(slideIndex), 4000);
+        // }
+
+        // var slides = document.getElementsByClassName("mySlides");
+
+        // if (slides.length > 0) {
+        //     showSlides(slideIndex);
+        //     myTimer = setInterval(plusSlides(1), 4000);
+        //     slideshowContainer = document.getElementsByClassName('slideshow-inner')[0];
+        //     slideshowContainer.addEventListener('mouseenter', pause)
+        //     slideshowContainer.addEventListener('mouseleave', resume)
+        // }
+
         next();
     }
 
